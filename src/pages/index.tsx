9344d9b6b43ad77a0593cb4b2bca3b07907696b6
Luiz Spinelli <luiz.luchi@gmail.com>
2021-09-25 19:24:15 -0300

    118n

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,16 @@
+import { GetServerSideProps } from "next";
+import { useTranslation } from "next-i18next";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+
 export default function Home() {
-  return <div>Home </div>;
+  const { t } = useTranslation();
+  return <div>{t("home")}</div>;
 }
+
+export const getServerSideProps: GetServerSideProps<any> = async ({
+  locale,
+}) => ({
+  props: {
+    ...(await serverSideTranslations(locale!, ["home"])),
+  },
+});
