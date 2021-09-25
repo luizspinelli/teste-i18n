6c7ce540559e27b28c33607faf1759e1bb90ffca
Luiz Spinelli <luiz.luchi@gmail.com>
2021-09-25 19:36:50 -0300

    118n

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,14 +1,14 @@
 module.exports = {
     i18n: {
-        defaultNS: "home",
-        fallbackLng: "pt",
-        localePath: typeof window === "undefined" ? "./public/locales" : "locales",
         locales: ["pt", "es", "en"],
         defaultLocale: "pt",
-        interpolation: {
-            escapeValue: false,
-        },
     },
+    defaultNS: "home",
+    fallbackLng: "pt",
+    interpolation: {
+        escapeValue: false,
+    },
+    localePath: typeof window === "undefined" ? "./public/locales" : "locales",
     react: {
         useSuspense: false,
         wait: true,
